4bdfe6a27df0a108736d45f4471bb3a6f05744b3
Shak <sanshakurt@gmail.com>
2025-10-13 18:03:08 +0330

    add and modify files for terminal menu

---
+++ app/terminal/_meta.ts
@@ -0,0 +1,20 @@
+import type { MetaRecord } from "nextra";
+
+const config: MetaRecord = {
+  "what-is-terminal": {
+    title: "What is Terminal? 🤔",
+    theme: {
+      breadcrumb: false,
+      copyPage: false,
+    },
+  },
+  "basic-commands": {
+    title: "Basic Terminal Commands ⌨️",
+    theme: {
+      breadcrumb: false,
+      copyPage: false,
+    },
+  },
+};
+
+export default config;
