6a94f186e48559966d34af604586f6d90e820bf8
Shak <sanshakurt@gmail.com>
2025-10-14 16:55:15 +0330

    add linux-essential menu and its sub-menus and config the menus files (_meta.ts files)

---
+++ app/linux-essential/_meta.ts
@@ -0,0 +1,34 @@
+import type { MetaRecord } from "nextra";
+
+const config: MetaRecord = {
+  introduction: {
+    title: "Introduction 🚀",
+    theme: {
+      breadcrumb: false,
+      copyPage: false,
+    },
+  },
+  root: {
+    title: "Root 🔑",
+    theme: {
+      breadcrumb: false,
+      copyPage: false,
+    },
+  },
+  "dual-boot": {
+    title: "Dual Boot 🔄",
+    theme: {
+      breadcrumb: false,
+      copyPage: false,
+    },
+  },
+  swap: {
+    title: "Swap 💾",
+    theme: {
+      breadcrumb: false,
+      copyPage: false,
+    },
+  },
+};
+
+export default config;
